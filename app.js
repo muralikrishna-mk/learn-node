81179e4fa262ce780b11f958f107f6a9d7c6d6f3
muralikrishna <63226273+muralikrishna-mk@users.noreply.github.com>
2021-02-28 13:26:54 +0530

    express completed

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
-const adminRoutes = require("./routes/admin")
-const shopRoutes = require("./routes/shop")
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const getRootPath = require("./util/path");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(getRootPath, "public")));
 
-app.use(adminRoutes)
-app.use(shopRoutes)
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(getRootPath, "views", "404.html"));
+});
 
 app.listen(4200);
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const path = require("path");
+
+const getRootPath = require("../util/path");
 
 const router = express.Router();
 
-router.get("/",(req , res, next) => {
-    res.send(`<h1>404</h1>`)
-})
+router.get("/", (req, res, next) => {
+  console.log(path.dirname(process.mainModule.filename));
+  res.sendFile(path.join(getRootPath, "views", "shop.html"));
+});
 
 module.exports = router;
